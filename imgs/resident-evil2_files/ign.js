fb29f06504f90281f6182f108d701745d87fb06d
jacjanowski <jjanow7@uic.edu>
2021-10-21 19:55:32 -0500

    new file. must fill out intended users

---
+++ imgs/resident-evil2_files/ign.js
@@ -0,0 +1,9 @@
+
+(function() {
+    var domain = '.ign.com'; var cc = 'US'; var akcc = 'US';
+    if (cc && document.cookie.indexOf('geoCC') == -1) {
+      document.cookie='geoCC='+cc+';path=/;domain='+domain;
+      document.cookie='geoCC='+cc+';path=/;domain=.ziffdavisinternational.com';
+    }
+})();
+    
